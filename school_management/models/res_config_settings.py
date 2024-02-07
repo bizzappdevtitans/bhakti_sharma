9cdc9369e9fe42c51b93033076c6119d4d113719
BAD <BAD@bizzappdev.com>
2024-02-07 12:05:34 +0530

    Changes in view

--- school_management/models/res_config_settings.py
+++ school_management/models/res_config_settings.py
@@ -4,19 +4,18 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    title_page_text = fields.Char(
-        string="Title Page Text",
+    title_page_text = fields.Integer(
+        string="Title",
         config_parameter="school.title_page_text",
-        default="Page Title Information",
     )
-    allowed_students = fields.Char(
-        string="Allowed Students", config_parameter="allowed_students"
-    )
-
-    deleted_students = fields.Char(
-        string="Deleted students", config_parameter="school.deleted_students", default=0
+    allowed_students = fields.Integer(
+        string="Allowed", config_parameter="allowed_students"
     )
 
-    available_rooms = fields.Char(
-        string="Available Room", config_parameter="class.available_rooms",default=0
+    deleted_students = fields.Integer(
+        string="Deleted ", config_parameter="school.deleted_students", default=0
+    )
+
+    available_rooms = fields.Integer(
+        string="Room", config_parameter="class.available_rooms", default=0
     )
